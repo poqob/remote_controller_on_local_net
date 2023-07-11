9a20edcd12a0172ac3a63dc6a286251bd3dbd994
Mustafa <mmermmus123@gmail.com>
2023-07-11 15:57:03 +0300

    minor updates, try catch +

--- rcoln_api/server.py
+++ rcoln_api/server.py
@@ -11,12 +11,20 @@ class UDPServer:
     def start_server(self):
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.bind((self.address, self.port))
+        print(f"Server started on: {self.address}:{self.port}")
 
         while True:
-            data, sender_address = self.udp_socket.recvfrom(self.buffer_size)
-            print("Received data:", data.decode('utf-8'))
+            try:
+                data, sender_address = self.udp_socket.recvfrom(self.buffer_size)
+                print("Received data:", data.decode("utf-8"))
+            except OSError as e:
+                # Handle the OSError if needed
+                # print("Error occurred:", e)
+                break
+        self.kill_server()
 
     def kill_server(self):
-        if self.udp_socket is not None:
+        if self.udp_socket:
             self.udp_socket.close()
+            self.udp_socket = None
             print("Server killed.")
