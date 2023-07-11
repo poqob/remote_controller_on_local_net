af87b418e0abfb3e2cb3383a84495ef7775cc9c3
Mustafa <mmermmus123@gmail.com>
2023-07-11 15:57:39 +0300

    config file added, cleare structure.

--- rcoln_api/server_application.py
+++ rcoln_api/server_application.py
@@ -1,49 +1,40 @@
 import tkinter as tk
-from server import UDPServer
-
-receiver_address = '0.0.0.0'
-receiver_port = 5100
-buffer_size = 1024
+from server_manager import ServerManager
+import threading
 
 
 class ServerManagerApp:
+    # fields
+    sm = ServerManager("rcoln_api\config.ini")
+    global root
+
     def __init__(self, root):
         self.root = root
-        self.server = None
-
         # Create a frame to hold the buttons
         self.frame = tk.Frame(root)
         self.frame.pack(pady=20)
 
         # Create the buttons
         self.start_button = tk.Button(
-            self.frame, text="Start Server", command=self.start_server)
+            self.frame, text="Start Server", command=self.start_server
+        )
         self.start_button.grid(row=0, column=0, padx=10)
 
         self.stop_button = tk.Button(
-            self.frame, text="Stop Server", command=self.stop_server)
+            self.frame, text="Stop Server", command=self.stop_server
+        )
         self.stop_button.grid(row=0, column=1, padx=10)
 
     def start_server(self):
-        if self.server is None:
-            self.server = UDPServer(
-                receiver_address, receiver_port, buffer_size)
-            self.server.start_server()
-        else:
-            print("Server is already running.")
+        # threading
+        threading.Thread(target=self.sm.start).start()
 
     def stop_server(self):
-        if self.server is not None:
-            self.server.kill_server()
-            self.server = None
-        else:
-            print("Server is not running.")
+        self.sm.stop()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Server Manager")
-
     app = ServerManagerApp(root)
-
     root.mainloop()
