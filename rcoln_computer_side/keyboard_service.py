32a1926b95ad9989bd9a017feb76c5a77035d292
Mustafa <mmermmus123@gmail.com>
2023-07-12 19:25:02 +0300

    controlling the input???

--- rcoln_computer_side/keyboard_service.py
+++ rcoln_computer_side/keyboard_service.py
@@ -1,11 +1,28 @@
 import keyboard
 
 
+# {KeyboardService}
+# keyable input types for keyboard service:
+
+# [regular input]
+# single character
+# multiple character -> parse them into single character
+
+# [special input]
+# hotkey
+
+
+# {Pipeline}
 # i'll use queue data structure.
 # keyboard service will accept requests and directs into a queue.
 # the service is listening the queue and state changes. then takes action according to the queue.
 # firstly i need a queue to receive requests(Model clss) and collect them.
 # then i have to listen this queue watch the changes.
+# make decision of press() or press_and_release() according to the pipeline's state.
+
+# [click behaviour]
+# tap modes: press and release[hotkey, single-multiple character], press(held)[single character]
+# the pipeline will indicate the tap mode.
 
 
 class KeyboardService:
@@ -13,9 +30,21 @@ class KeyboardService:
         raise TypeError("Static classes can't be instantied")
 
     @staticmethod
-    def press(key):
+    def hotkey(hotkey):
+        # keyboard.press_and_release("windows+d")
+        KeyboardService.singlePressAndRelease(hotkey)
+
+    # makes single character press (HELD)
+    @staticmethod
+    def singlePress(key):
         keyboard.press(key)
 
+    # makes key release up to given key
     @staticmethod
-    def hotkey():
-        keyboard.press_and_release("windows+d")
+    def releaseKey(key):
+        keyboard.release(key)
+
+    # makes single character press and release (PAR, press and release)
+    @staticmethod
+    def singlePressAndRelease(key):
+        keyboard.press_and_release(key)
