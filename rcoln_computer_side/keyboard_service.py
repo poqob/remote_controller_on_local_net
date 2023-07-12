b574a0023749e70f0d6b9d7bbca51cdfd0f86e59
Mustafa <mmermmus123@gmail.com>
2023-07-12 15:36:11 +0300

    keyboard service tested. status ok.

--- rcoln_computer_side/keyboard_service.py
+++ rcoln_computer_side/keyboard_service.py
@@ -1,9 +1,21 @@
 import keyboard
 
 
-class KeyboardService:
-    # fields
+# i'll use queue data structure.
+# keyboard service will accept requests and directs into a queue.
+# the service is listening the queue and state changes. then takes action according to the queue.
+# firstly i need a queue to receive requests(Model clss) and collect them.
+# then i have to listen this queue watch the changes.
 
-    # constructor
-    def __init__(self) -> None:
-        pass
+
+class KeyboardService:
+    def __new__(cls):
+        raise TypeError("Static classes can't be instantied")
+
+    @staticmethod
+    def press(key):
+        keyboard.press(key)
+
+    @staticmethod
+    def hotkey():
+        keyboard.press_and_release("windows+d")
