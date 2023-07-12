16b1975f090455d3d8344117197773b53e946a15
Mustafa <mmermmus123@gmail.com>
2023-07-12 15:34:28 +0300

    temux test file updated. send dynamic data

--- rcoln_mobile_side/test_view/test_termux.py
+++ rcoln_mobile_side/test_view/test_termux.py
@@ -5,13 +5,16 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Replace with the desired destination IP address
-destination_address = '192.168.113.224' #address of the computer server launch.
+destination_address = "192.168.113.224"  # address of the computer server launch.
 destination_port = 5100  # Replace with the port in config file.
 
 
 def send():
-    data="hi from termux!"
-    bytes_data = data.encode('utf-8')
+    serv = input("services: [k,m,o]; k:keyboard, m:mouse, o:other \nservice:")
+    data = f'{{"_service": "{serv}", "_key": "s"}}'
+
+    bytes_data = data.encode("utf-8")
     sock.sendto(bytes_data, (destination_address, destination_port))
+
 
 send()
