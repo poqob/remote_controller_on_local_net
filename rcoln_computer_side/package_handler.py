3ded1c13b52d1a4982500a3f1f9e568f0a50f7af
Mustafa <mmermmus123@gmail.com>
2023-07-13 21:05:46 +0300

    package handler created

---
+++ rcoln_computer_side/package_handler.py
@@ -0,0 +1,32 @@
+from model import Model
+from servicesEnum import Services
+
+
+# solve packages to models then return package service type and package's model
+class PackageHandler:
+    # fields
+
+    # new
+    def __new__(cls):
+        raise TypeError("Static classes can't be instantied")
+
+    @staticmethod
+    def handle(data, sender_address):
+        model = data.decode("utf-8")
+        model = Model.fromJson(model)
+        service = None
+        match model._service:
+            case Services.KEYBOARD.value:
+                print("keyboard input")
+                service = Services.KEYBOARD
+                # ServiceHandler.keyboard(model=model)
+            case Services.MOUSE.value:
+                print("mouse input")
+                service = Services.MOUSE
+            case Services.OTHER.value:
+                print("other input")
+                service = Services.OTHER
+            case _:
+                print(f"undefine input: {model._service}")
+                service = Services.OTHER
+        return service, model
